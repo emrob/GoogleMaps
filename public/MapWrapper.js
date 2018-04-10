833fbf54ba9886c2c23f4a63295a4b38adf54d87
Emma Roberts <emrsixtyseven@gmail.com>
2018-04-10 18:23:01 +0100

    marker added

--- public/MapWrapper.js
+++ public/MapWrapper.js
@@ -5,3 +5,11 @@ const MapWrapper = function(container, coords, zoom){
   });
   this.markers = [];
 }
+
+MapWrapper.prototype.addMarker = function(coordinates){
+  const marker = new google.maps.Marker({
+    position: coordinates,
+    map: this.googleMap
+  })
+  this.markers.push(marker);
+}
