446fdabe776e539b3087a6c1e04f8d97443cf6ad
Emma Roberts <emrsixtyseven@gmail.com>
2018-04-10 18:17:45 +0100

    map showing

---
+++ public/app.js
@@ -0,0 +1,12 @@
+const initialize = function(){
+
+const container = document.getElementById('main-map');
+const startPoint = {lat:51.124703, lng: 0.0084316}
+const zoom = 14;
+
+const map = new MapWrapper(container, startPoint, zoom);
+
+map.addMarker(startPoint);
+}
+
+window.addEventListener('DOMContentLoaded', initialize)
